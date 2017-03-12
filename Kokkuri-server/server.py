886ee8e2505509a36788f83db9a743f41d0beab5
lightless <lightless@foxmail.com>
2017-03-12 23:23:46 +0800

    add two dir

---
+++ Kokkuri-server/server.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+# coding: utf-8
+# Created by PyCharm
+# Project: Kokkuri
+# Filename: server.py
+# Time: 2017/3/12
+
+
+__author__ = 'lightless'
+__email__ = 'root@lightless.me'
+
